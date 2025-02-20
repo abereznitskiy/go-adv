72aea245b553c4708619c45470f42d3c42f4c706
Aleksandr Bereznitskii <abereznitskiy31@gmail.com>
2025-02-20 20:53:25 +0300

    feat(1-concurrency): Add fixes

--- 1-concurrency/main.go
+++ 1-concurrency/main.go
@@ -6,23 +6,21 @@ import (
 	"sync"
 )
 
+const (
+	nums            = 10
+	randomNumsCount = 100
+)
+
 func main() {
-	NUMS_COUNT := 10
-	numChan := make(chan int, NUMS_COUNT)
-	squareChan := make(chan int, NUMS_COUNT)
+	numChan := make(chan int, nums)
+	squareChan := make(chan int, nums)
 	var wg sync.WaitGroup
 
 	wg.Add(1)
-	go createSlice(NUMS_COUNT, numChan, &wg)
+	go createSlice(nums, numChan, &wg)
 
 	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		for num := range numChan {
-			squareNum(num, squareChan)
-		}
-		close(squareChan)
-	}()
+	go calculateSquares(numChan, squareChan, &wg)
 
 	wg.Wait()
 	for value := range squareChan {
@@ -33,10 +31,18 @@ func main() {
 func createSlice(count int, dataChan chan int, wg *sync.WaitGroup) {
 	defer wg.Done()
 	for i := 0; i < count; i++ {
-		randomNumberInRange := rand.Intn(100)
+		randomNumberInRange := rand.Intn(randomNumsCount)
 		dataChan <- randomNumberInRange
 	}
 	close(dataChan)
+}
+
+func calculateSquares(inputChan chan int, outputChan chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for num := range inputChan {
+		squareNum(num, outputChan)
+	}
+	close(outputChan)
 }
 
 func squareNum(num int, dataChan chan int) {
